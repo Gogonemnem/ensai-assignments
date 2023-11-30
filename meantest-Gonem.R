6a69572a26d766636e9318b5537274a5eba72e15
Gonem <glau.github@pm.me>
2023-11-30 09:25:11 +0100

    code for assignment 2

--- meantest-Gonem.R
+++ meantest-Gonem.R
@@ -1,10 +1,13 @@
+R_REPLICATIONS <- 200
+N_SAMPLES <- 100
+K_DESIGN_POINTS <- 20
 J_BASES <- 100
-N_SAMPLES <- 300
-K_TIMES <- 250
+L_TIMES <- 250
 MC_SAMPLES <- 500
-R_REPLICATIONS <- 500
-set.seed(2023)
+J_TRUNCATED_BASES <- 50
 
+
+set.seed(2023)
 
 
 ### Utility and Mathematical Functions
@@ -16,7 +19,7 @@ sinusoidal_mean_fn <- function(time, amplitude = 1, frequency = 1, phase = 0) {
   amplitude * sin(2 * pi * frequency * time + phase)
 }
 
-error_fn <- function(time, sigma = 1) {
+error_fn <- function(time, sigma = 0.5) {
   rnorm(1, mean = 0, sd = sigma)
 }
 
@@ -46,12 +49,12 @@ eigen_functions_bm <- function(num_bases) {
 
 
 ### Data Generation Functions
-# Function to simulate design points based on Poisson or fixed K
-simulate_design_points <- function(num_functions, num_times, is_poisson = FALSE) {
+# Function to generate a single set of design points based on Poisson or fixed K
+generate_single_design_points <- function(num_times, is_poisson = FALSE) {
   if (is_poisson) {
-    sapply(seq_len(num_functions), function(i) sort(runif(rpois(1, lambda = num_times), 0, 1)))
+    sort(runif(rpois(1, lambda = num_times), 0, 1))
   } else {
-    sapply(seq_len(num_functions), function(i) sort(runif(num_times, 0, 1)))
+    sort(runif(num_times, 0, 1))
   }
 }
 
@@ -69,13 +72,16 @@ score_generator <- function(num_samples, eigen_values) {
 
 # Function to generate observations with noise at design points
 generate_observations <- function(observation_functions, num_times, is_poisson = FALSE) {
-  design_points <- simulate_design_points(length(observation_functions), num_times, is_poisson)
+  observations_with_design_points <- lapply(observation_functions, function(f) {
+    # Generate design points for this function
+    design_points <- generate_single_design_points(num_times, is_poisson)
+    # Generate observations at these design points
+    observations <- sapply(design_points, f)
 
-  observations <- lapply(seq_along(observation_functions), function(i) {
-    sapply(design_points[[i]], observation_functions[[i]])
+    list(observations = observations, design_points = design_points)
   })
 
-  list(observations = observations, design_points = design_points)
+  observations_with_design_points
 }
 
 
@@ -83,14 +89,6 @@ generate_observations <- function(observation_functions, num_times, is_poisson =
 
 ### Functional Transformation and Analysis Functions
 # Function to generate a time-specific function based on scores and eigenfunctions
-function_generator_bm <- function(mean_fn, scores, eigen_fns) {
-  function(time) {
-    specific <- apply(scores, 1, function(row) sum(mapply(FUN = function(eigen_fn, elem) eigen_fn(time) * elem, eigen_fns, row)))
-    common <- mean_fn(time)
-    return(common + specific)
-  }
-}
-
 function_generator_bm <- function(mean_fn, scores, eigen_fns) {
   apply(scores, 1, function(score_row) {
     function(time) {
@@ -122,10 +120,38 @@ observation_function_generator <- function(functions, error_fn) {
 }
 
 
+# Function to generate linear interpolation functions for each observation
+estimate_interpolated_functions <- function(observations_with_design_points) {
+  lapply(observations_with_design_points, function(data) {
+    observations <- data$observations
+    design_points <- data$design_points
+    approxfun(design_points, observations, method = "linear", rule = 2)
+  })
+}
+
+# Function to generate Nadaraya-Watson smoothed functions
+estimate_smoothed_functions <- function(observations_with_design_points, bandwidth, kernel) {
+  lapply(observations_with_design_points, function(data) {
+    observations <- data$observations
+    design_points <- data$design_points
+
+    # Create function using a Gaussian kernel
+    function(time) {
+      distances <- (time - design_points) / bandwidth
+      weights <- kernel(distances)
+      # weights <- dnorm(distances, mean = 0, sd = 1)
+      sum(weights * observations) / sum(weights)
+    }
+  })
+}
+
+
 ### Monte Carlo and Statistical Functions
 # Function to approximate the norm of the squared difference between empirical and true mean functions
 mc_norm <- function(mc_samples, empirical_mean_fn, mean_fn) {
-  sampled_norms <- sapply(mc_samples, function(time) {(empirical_mean_fn(time) - mean_fn(time))^2})
+  sampled_norms <- sapply(mc_samples, function(time) {
+    (empirical_mean_fn(time) - mean_fn(time))^2
+  })
   return(mean(sampled_norms))
 }
 
@@ -160,26 +186,31 @@ grid_times <- function(n_samples, min_time, max_time) {
 }
 
 # Function to replicate the test norm, generating new samples and functions each time
-replicate_test_norm <- function(num_times, time_sampler, num_samples, eigen_values, eigen_functions, mean_fn, test_mean_fn = NULL) {
+replicate_test_norm <- function(num_points, num_times, time_sampler, num_samples, eigen_values, eigen_functions, function_estimator, mean_fn, is_poisson = TRUE, test_mean_fn = NULL) {
   if (is.null(test_mean_fn)) {
     test_mean_fn <- mean_fn
   }
-
-  # Generate new sampled times for each replication
-  sampled_times <- time_sampler(num_times, 0, 1)
 
   # Generate new scores and functions for each empirical mean function
   scores <- score_generator(num_samples, eigen_values)
   functions <- function_generator_bm(mean_fn, scores, eigen_functions)
   observation_functions <- observation_function_generator(functions, error_fn)
-  
-  empirical_mean_fn <- empirical_mean_function(functions)
+
+  observations <- generate_observations(observation_functions, num_points, is_poisson = TRUE)
+  estimated_functions <- function_estimator(observations)
+
+  empirical_mean_fn <- empirical_mean_function(estimated_functions)
+
+  # Generate new sampled times for each replication
+  sampled_times <- time_sampler(num_times, 0, 1)
 
   # Compute test norm
-  # test_norm(num_samples, sampled_times, empirical_mean_fn, test_mean_fn)
+  test_norm(num_samples, sampled_times, empirical_mean_fn, test_mean_fn)
 }
 
-replicate_test_norm(K_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, zero_mean_fn)
+gaussianNW <- function(obs) estimate_smoothed_functions(obs, 0.1, dnorm)
+
+
 
 ### Main Execution
 # Brownian Motion simulation setup
@@ -190,41 +221,46 @@ eigen_functions <- eigen_functions_bm(J_BASES)
 library(parallel)
 num_cores <- detectCores() * 2 / 3
 
-
-zero_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, zero_mean_fn), mc.cores = num_cores)
-sinusoidal_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, sinusoidal_mean_fn), mc.cores = num_cores)
-sinusoidal_on_zero_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, sinusoidal_mean_fn, zero_mean_fn), mc.cores = num_cores)
-
-
-# Generate a sequence of probabilities
+# Calculating theoretical quantiles
 probs_seq <- seq(0, 1, by = 0.01)
+truncated_eigen_values <- eigen_values[seq_len(J_TRUNCATED_BASES)]
+quantiles <- mc_quantiles(MC_SAMPLES, truncated_eigen_values, probs_seq)
 
-# Calculating and comparing quantiles
-quantiles <- mc_quantiles(MC_SAMPLES, eigen_values, probs_seq)
+
+zero_linear_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_DESIGN_POINTS, L_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, estimate_interpolated_functions, zero_mean_fn), mc.cores = num_cores)
+zero_smoothed_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_DESIGN_POINTS, L_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, gaussianNW, zero_mean_fn), mc.cores = num_cores)
+sinusoidal_linear_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_DESIGN_POINTS, L_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, estimate_interpolated_functions, sinusoidal_mean_fn), mc.cores = num_cores)
+sinusoidal_smoothed_test_norm_results <- mclapply(seq_len(R_REPLICATIONS), function(i) replicate_test_norm(K_DESIGN_POINTS, L_TIMES, grid_times, N_SAMPLES, eigen_values, eigen_functions, gaussianNW, sinusoidal_mean_fn), mc.cores = num_cores)
 
 
-zero_results <- sapply(quantiles, function(quantile) mean(zero_test_norm_results <= quantile))
-zero_results
+zero_linear_results <- sapply(quantiles, function(quantile) mean(zero_linear_test_norm_results <= quantile))
+zero_smoothed_results <- sapply(quantiles, function(quantile) mean(zero_smoothed_test_norm_results <= quantile))
+sinusoidal_linear_results <- sapply(quantiles, function(quantile) mean(sinusoidal_linear_test_norm_results <= quantile))
+sinusoidal_smoothed_results <- sapply(quantiles, function(quantile) mean(sinusoidal_smoothed_test_norm_results <= quantile))
 
-sinusoidal_results <- sapply(quantiles, function(quantile) mean(sinusoidal_test_norm_results <= quantile))
-sinusoidal_results
 
-sinusoidal_on_zero_results <- sapply(quantiles, function(quantile) mean(sinusoidal_on_zero_test_norm_results <= quantile))
-sinusoidal_on_zero_results
+zero_linear_results
+zero_smoothed_results
+sinusoidal_linear_results
+sinusoidal_smoothed_results
 
 
 # Calculate theoretical & empirical quantiles of test_norm_results
-zero_empirical_quantiles <- quantile(unlist(zero_test_norm_results), probs = probs_seq)
-sinusoidal_empirical_quantiles <- quantile(unlist(sinusoidal_test_norm_results), probs = probs_seq)
-sinusoidal_on_zero_empirical_quantiles <- quantile(unlist(sinusoidal_on_zero_test_norm_results), probs = probs_seq)
-theoretical_quantiles <- mc_quantiles(MC_SAMPLES, eigen_values, probs_seq)
+zero_linear_empirical_quantiles <- quantile(unlist(zero_linear_test_norm_results), probs = probs_seq)
+zero_smoothed_empirical_quantiles <- quantile(unlist(zero_smoothed_test_norm_results), probs = probs_seq)
+sinusoidal_linear_empirical_quantiles <- quantile(unlist(sinusoidal_linear_test_norm_results), probs = probs_seq)
+sinusoidal_smoothed_empirical_quantiles <- quantile(unlist(sinusoidal_smoothed_test_norm_results), probs = probs_seq)
+
 
 # Creating the QQ plots
-plot(theoretical_quantiles, zero_empirical_quantiles, main = "QQ Plot for Zero Mean", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
+plot(quantiles, zero_linear_empirical_quantiles, main = "QQ Plot for Zero Mean Linearly Interpolated", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
 abline(0, 1, col = "red")
 
-plot(theoretical_quantiles, sinusoidal_empirical_quantiles, main = "QQ Plot for Sinusoidal Mean", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
+plot(quantiles, zero_smoothed_empirical_quantiles, main = "QQ Plot for Zero Mean Gaussian NW Smoothed", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
 abline(0, 1, col = "red")
 
-plot(theoretical_quantiles, sinusoidal_on_zero_empirical_quantiles, main = "QQ Plot for Sinusoidal Mean Testing Zero Mean", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
+plot(quantiles, sinusoidal_linear_empirical_quantiles, main = "QQ Plot for Sinusoidal Mean Linearly Interpolated", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
+abline(0, 1, col = "red")
+
+plot(quantiles, sinusoidal_linear_empirical_quantiles, main = "QQ Plot for Sinusoidal Mean Gaussian NW Smoothed", xlab = "Theoretical Quantiles", ylab = "Empirical Quantiles", pch = 19)
 abline(0, 1, col = "red")
